4a460e2898e951d4e0e8f60f14d67a490cd65dd6
HazielHc <haziel.hernandez@upnay.edu.mx>
2025-08-05 12:05:36 -0700

    Demo Nivel 1
    Se agrego una prueba de lo que será el nivel uno, colocando un primer TileMap de ejemplo y comprobando su colisión con el personaje

--- PlayerMovement.gd
+++ PlayerMovement.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const DOUBLE_JUMP_VELOCITY = -400.0
+var can_double_jump = false
 
-@onready var animationPlayer= $AnimationPlayer
+@onready var animationPlayer = $AnimationPlayer
 @onready var sprite2D = $Sprite2D
-
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,8 +14,15 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			# Regular jump from the ground
+			velocity.y = JUMP_VELOCITY
+			can_double_jump = true
+		elif can_double_jump:
+			# Double jump
+			velocity.y = DOUBLE_JUMP_VELOCITY
+			can_double_jump = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -26,22 +33,22 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-	
+
 	animations(direction)
-	
-	if direction==1:
-		sprite2D.flip_h = false 
+
+	if direction == 1:
+		sprite2D.flip_h = false
 	elif direction == -1:
 		sprite2D.flip_h = true
-		
+
 func animations(direction):
 	if is_on_floor():
-		if direction==0:
+		if direction == 0:
 			animationPlayer.play("Idle")
 		else:
 			animationPlayer.play("run")
 	else:
-		if velocity.y<0:
+		if velocity.y < 0:
 			animationPlayer.play("jump")
-		elif velocity.y>0: 
+		elif velocity.y > 0:
 			animationPlayer.play("fall")
